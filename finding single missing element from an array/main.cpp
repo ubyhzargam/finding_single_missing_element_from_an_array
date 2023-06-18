3cb46bb7509baf79e956e97693f19ea0f4a02b12
ubyhzargam <ubyhzargam@gmail.com>
2023-06-18 12:59:14 +0530

    Update main.cpp

--- finding single missing element from an array/main.cpp
+++ finding single missing element from an array/main.cpp
@@ -6,9 +6,19 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    int arr[20],l;
+    cout<<"Enter the number of elements in the array : ";
+    cin>>l;
+    cout<<"Enter the elements in the array : "<<endl;
+    for(int i=0;i<l;i++)
+        cin>>arr[i];
+    for(int i=0;i<l-1;i++)
+    {
+        if(arr[i+1]-arr[i]!=1)
+            cout<<"The missing element in the array is : "<<arr[i]+1<<endl;
+    }
     return 0;
 }
